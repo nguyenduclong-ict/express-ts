dc47b7146ab39911886cb54074b69f7a97d8e8e6
Nguyễn Đức Long <longnd@rabiloo.com.vn>
2020-04-16 09:13:09 +0700

    update version 0.0.1

--- dist/routes/index.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const index_handle_1 = require("./index.handle");
-const router = express_1.Router();
-router.get('/', index_handle_1.handleSayHello);
-exports.default = router;
